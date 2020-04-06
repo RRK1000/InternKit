fede18dde340d403a8a3c1c3cfdd65e4199b8395
Aniket Kaulavkar <aniket.kaulavkar@gmail.com>
2020-04-06 22:05:42 +0530

    Added utils for login and setToken

--- frontend/src/Components/HomePage/index.js
+++ frontend/src/Components/HomePage/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "@material-ui/core/Container";
 import StudentDashBoard from "../StudentDashboard";
 import { makeStyles } from "@material-ui/core/styles";
+const isLoggedIn = require("../../util/auth").isLoggedIn;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,13 +12,12 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Home() {
   const classes = useStyles();
-  var isLoggedIn = false;
   const loggedInComponent = <StudentDashBoard />;
   const loggedOutComponent = <div>Default Home Page</div>;
 
   return (
     <Container className={classes.root} maxWidth="lg">
-      {isLoggedIn ? loggedInComponent : loggedOutComponent}
+      {isLoggedIn() ? loggedInComponent : loggedOutComponent}
     </Container>
   );
 }
