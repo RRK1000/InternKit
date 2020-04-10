453505ad321144339c8c864a16aaad1415baac56
Vishnu S Murali <muralisvishnu@gmail.com>
2020-04-10 19:42:53 +0530

    working wordcloud

--- intelligent_component/cloud.py
+++ intelligent_component/cloud.py
@@ -7,5 +7,7 @@ def create_word_cloud(string):
    #maskArray = npy.array(Image.open("cloud.png"))
    cloud =WordCloud(background_color = "white", max_words = 200,stopwords = set(STOPWORDS)).generate(string)
    cloud.to_file("wordCloud.png")
+   for i in cloud.layout_:
+      print(i[0][1],i[1])
 dataset = dataset.lower()
 create_word_cloud(dataset)
