5840dd73a29ef8715fd980618d0a8fa978274150
Rishi Ravikumar <rishiravi.k98@gmail.com>
2020-04-14 17:17:58 +0530

    Added Dynamic inputs to StudentProfile

--- frontend/src/Components/AddProfilePage/StudentProfile.js
+++ frontend/src/Components/AddProfilePage/StudentProfile.js
@@ -4,7 +4,10 @@ import Container from "@material-ui/core/Container";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
 import { makeStyles } from "@material-ui/core/styles";
 import { useGetAndSet, useStoreValue } from "react-context-hook";
 
@@ -29,8 +32,19 @@ const useStyles = makeStyles((theme) => ({
         marginLeft: theme.spacing(1),
     },
     skills: {
-        width: "20ch",
+        "& .MuiTextField-root": {
+            width: "30ch",
+        },
     },
+    value: {
+        "& .MuiTextField-root": {
+            width: "10ch",
+        },
+    },
+    paper:{
+        marginTop: theme.spacing(3),
+        marginBottom: theme.spacing(1)
+    }
 }));
 
 export default function StudentProfile() {
@@ -44,13 +58,14 @@ export default function StudentProfile() {
     const [branch, setBranch] = useState("");
     const [github, setGithub] = useState("");
     const [linkedIn, setLinkedIn] = useState("");
-    const [skills, setSkills] = useState("");
+    // const [skills, setSkills] = useState("");
     //   const [pdescription, setPdescription] = useState("");
 
     const token = useStoreValue("isLoggedIn");
     const username = useStoreValue("username");
-    const usertype = useStoreValue('usertype');
-    const [hasProfile, setHasProfile] = useGetAndSet('hasProfile');
+    const usertype = useStoreValue("usertype");
+    const [isLoggedIn] = useGetAndSet("isLoggedIn");
+    const [hasProfile, setHasProfile] = useGetAndSet("hasProfile");
 
     const handleChange = (e) => {
         if (e.target.name === "dob") setDob(e.target.value);
@@ -62,30 +77,167 @@ export default function StudentProfile() {
         else if (e.target.name === "branch") setBranch(e.target.value);
         else if (e.target.name === "github") setGithub(e.target.value);
         else if (e.target.name === "linkedIn") setLinkedIn(e.target.value);
-        else if (e.target.name === "skills") setSkills(e.target.value);
+        // else if (e.target.name === "skills") setSkills(e.target.value);
         // else if (e.target.name === "pdescription") setPdescription(e.target.value);
     };
+    const [skill, setSkill] = useState({ val: [] });
+    const [values, setValues] = useState({ val: [] });
 
+    const createSkill = () => {
+        let x;
+        return skill.val.map((el, i) => (
+            <Grid container key={i}>
+                <Grid item xs={7} className={classes.skills}>
+                    <TextField
+                        id={i}
+                        label="Skill"
+                        className={classes.skills}
+                        variant="outlined"
+                        value={el}
+                        onChange={handleSkillChange.bind(i)}
+                    />
+                </Grid>
+                <Grid item xs={3} className={classes.value}>
+                    <TextField
+                        id={i}
+                        label="1-10"
+                        variant="outlined"
+                        value={x}
+                        onChange={handleValueChange.bind(i)}
+                    />
+                </Grid>
+                <IconButton
+                    aria-label="delete"
+                    className={classes.button}
+                    onClick={removeSkill.bind(i)}
+                >
+                    <DeleteIcon fontSize="small" />
+                </IconButton>
+            </Grid>
+        ));
+    };
+
+    const handleSkillChange = (event) => {
+        let vals = [...skill.val];
+
+        vals[event.target.id] = event.target.value;
+        console.log(vals);
+        setSkill({ val: vals });
+    };
+
+    const handleValueChange = (event) => {
+        let vals = [...values.val];
+
+        vals[event.target.id] = event.target.value;
+        console.log(vals);
+        setValues({ val: vals });
+    };
+
+    const addSkill = () => {
+        setSkill({ val: [...skill.val, ""] });
+        setValues({ val: [...values.val, ""] });
+    };
+
+    const removeSkill = (i) => {
+        let vals = [...skill.val];
+        vals.splice(this, 1);
+        setSkill({ val: vals });
+
+        let vals1 = [...values.val];
+        vals1.splice(this, 1);
+        setValues({ val: vals });
+    };
+
+    const [project, setProject] = useState({ val: [] });
+    const [desc, setDesc] = useState({ val: [] });
+
+    const createProject = () => {
+        let x;
+        return project.val.map((el, i) => (
+            <Paper className={classes.paper} elevation={3} key={i}>
+                <TextField
+                    id={i}
+                    label="Project Title"
+                    className={classes.skills}
+                    variant="outlined"
+                    value={el}
+                    onChange={handleProjectChange.bind(i)}
+                />
+                <TextField
+                    id={i}
+                    label="Project Description"
+                    variant="outlined"
+                    value={x}
+                    onChange={handleDescChange.bind(i)}
+                />
+                <IconButton
+                    aria-label="delete"
+                    className={classes.button}
+                    onClick={removeProject.bind(i)}
+                >
+                    <DeleteIcon fontSize="small" />
+                </IconButton>
+            </Paper>
+        ));
+    };
+
+    const handleProjectChange = (event) => {
+        let vals = [...project.val];
+
+        vals[event.target.id] = event.target.value;
+        console.log(vals);
+        setProject({ val: vals });
+    };
+
+    const handleDescChange = (event) => {
+        let vals = [...desc.val];
+
+        vals[event.target.id] = event.target.value;
+        console.log(vals);
+        setDesc({ val: vals });
+    };
+
+    const addProject = () => {
+        setProject({ val: [...project.val, ""] });
+        setDesc({ val: [...desc.val, ""] });
+    };
+
+    const removeProject = (i) => {
+        let vals = [...project.val];
+        vals.splice(this, 1);
+        setProject({ val: vals });
+
+        let vals1 = [...desc.val];
+        vals1.splice(this, 1);
+        setDesc({ val: vals });
+    };
     const createAcc = () => {
+        var skillsList = "";
+        for (var i = 0; i < skill.val.length; i++) {
+            skillsList += skill.val[i] + "-" + values.val[i] + "\n";
+        }
+
+        var projList = "";
+        for (var i = 0; i < project.val.length; i++) {
+            projList += project.val[i] + "\n" + desc.val[i] + "\n";
+        }
         const data = Object.assign(
             {},
             {
                 username,
                 dob,
-                token,
+                token: isLoggedIn,
                 email,
                 phone,
                 education: grade12 + ";" + gpa,
                 college,
                 branch,
                 snetwork: github + ";" + linkedIn,
-                skills: "python-5;java-6;ml-9",
-                pdescription:
-                    "ProjectTitle1#projectdescription;ProjectTitle2#projectdescription;ProjectTitle3#projectdescription",
+                skills: skillsList,
+                pdescription: projList,
                 usertype,
             }
         );
-        console.log(data);
         fetch("http://127.0.0.1:5000/api/v1/addprofile", {
             method: "POST",
             mode: "cors",
@@ -109,7 +261,7 @@ export default function StudentProfile() {
                 alert(error);
             });
     };
-    if (hasProfile) return <Redirect to='/' />;
+    if (hasProfile) return <Redirect to="/" />;
 
     return (
         <Container maxWidth="sm">
@@ -120,7 +272,7 @@ export default function StudentProfile() {
                 className={classes.root}
                 noValidate
                 autoComplete="off"
-            // onSubmit={createAcc}
+                // onSubmit={createAcc}
             >
                 <Grid container>
                     <Grid container item>
@@ -239,17 +391,32 @@ export default function StudentProfile() {
                             Skills
                         </Typography>
                     </Grid>
+                    {createSkill()}
+                    <Button
+                        variant="outlined"
+                        size="small"
+                        color="primary"
+                        className={classes.button}
+                        onClick={addSkill}
+                    >
+                        Add Skill
+                    </Button>
+
                     <Grid container item>
-                        <TextField
-                            id="outlined1"
-                            className={classes.skills}
-                            label="Add Skill"
-                            name="skills"
-                            value={skills}
-                            variant="outlined"
-                            onChange={handleChange}
-                        />
+                        <Typography className={classes.caption} variant="h4">
+                            Projects
+                        </Typography>
                     </Grid>
+                    {createProject()}
+                    <Button
+                        variant="outlined"
+                        size="small"
+                        color="primary"
+                        className={classes.button}
+                        onClick={addProject}
+                    >
+                        Add Project
+                    </Button>
 
                     <Grid container item>
                         <Button
