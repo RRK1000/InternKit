0349dac98cb00fc6e9ebbcd2a98612211fe34905
Rishi Ravikumar <rishiravi.k98@gmail.com>
2020-04-15 13:45:30 +0530

    Added internship create form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { withStore } from 'react-context-hook';
+import { withStore } from "react-context-hook";
 import Header from "./Components/Header";
 import Home from "./Components/HomePage";
 import SignIn from "./Components/SignInPage";
@@ -8,44 +8,47 @@ import SignUp from "./Components/SignUpPage";
 import Profile from "./Components/ProfilePage";
 import Logout from "./Components/Logout";
 import AddProfile from "./Components/AddProfilePage";
+import AddInternship from "./Components/AddInternship";
 
 const initialState = Object.assign(
-  {
-    isLoggedIn: false,
-    username: null
-  },
-  JSON.parse(localStorage.getItem('state'))); // Using localStorage to persist on reload
+    {
+        isLoggedIn: false,
+        username: null,
+    },
+    JSON.parse(localStorage.getItem("state"))
+); // Using localStorage to persist on reload
 
 const storeConfig = {
-  listener: state => {
-    console.log('state changed', state)
-    // persist state to localStorage
-    localStorage.setItem('state', JSON.stringify(state))
-  },
-  logging: process.env.NODE_ENV !== 'production'
+    listener: (state) => {
+        console.log("state changed", state);
+        // persist state to localStorage
+        localStorage.setItem("state", JSON.stringify(state));
+    },
+    logging: process.env.NODE_ENV !== "production",
 };
 
 function App() {
-  return (
-    <Router>
-      <Header />
+    return (
+        <Router>
+            <Header />
 
-      <Switch>
-        <Route exact path="/" component={Home} />
+            <Switch>
+                <Route exact path="/" component={Home} />
 
-        <Route path="/signin" component={SignIn} />
+                <Route path="/signin" component={SignIn} />
 
-        <Route path="/signup" component={SignUp} />
+                <Route path="/signup" component={SignUp} />
 
-        <Route path="/addprofile" component={AddProfile} />
+                <Route path="/addprofile" component={AddProfile} />
 
-        <Route path="/profile" component={Profile} />
+                <Route path="/profile" component={Profile} />
 
-        <Route path="/logout" component={Logout} />
+                <Route path="/addinternship" component={AddInternship} />
 
-      </Switch>
-    </Router>
-  );
+                <Route path="/logout" component={Logout} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default withStore(App, initialState, storeConfig);
---
+++ frontend/src/Components/AddInternship/index.js
@@ -0,0 +1,190 @@
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import Container from "@material-ui/core/Container";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import { useGetAndSet, useStoreValue } from "react-context-hook";
+
+const setToken = require("../../util/auth").setToken;
+
+const useStyles = (theme) => ({
+    root: {
+        "& .MuiTextField-root": {
+            margin: theme.spacing(1),
+            width: "50ch",
+        },
+    },
+    heading: {
+        margin: theme.spacing(2),
+    },
+    button: {
+        marginLeft: theme.spacing(1),
+        marginTop: theme.spacing(1),
+    },
+    caption: {
+        marginLeft: theme.spacing(1),
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        marginLeft: theme.spacing(2),
+        marginTop: theme.spacing(3),
+    },
+});
+
+function AddInternship(props) {
+    const usertype = "internship";
+
+    // const token = useStoreValue("isLoggedIn");
+    const username = useStoreValue("username");
+
+    // const [emp_name, setEmp_name] = useState("");
+    const [itr_name, setItr_name] = useState("");
+    const [stipend, setStipend] = useState("");
+    const [branch, setBranch] = useState("");
+    const [city, setCity] = useState("");
+    const [description, setDescription] = useState("");
+    const [gpa, setGpa] = useState("");
+    const [hasCreated, setHasCreated] = useState("");
+
+    const onSubmit = () => {
+        const data = {
+            emp_name: username,
+            itr_name,
+            stipend,
+            branch,
+            city,
+            description,
+            gpa,
+            usertype,
+        };
+        console.log(data);
+        fetch("http://127.0.0.1:5000/api/v1/add_internship_scholarship", {
+            method: "PUT",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => {
+                console.log(response);
+                if (response.ok) return response.json();
+                else throw Error(response.status + " " + response.statusText);
+            })
+            .then((data) => {
+                console.log(data);
+                setHasCreated(true);
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    };
+    const { classes } = props;
+
+    return hasCreated ? (
+        <Redirect to="/" />
+    ) : (
+        <Container maxWidth="sm">
+            <Typography className={classes.heading} variant="h4">
+                Add Internship
+            </Typography>
+            <form
+                className={classes.root}
+                noValidate
+                autoComplete="off"
+                // onSubmit={this.onSubmit}
+            >
+                <Grid container>
+                    <Grid container item>
+                        <TextField
+                            required
+                            id="outlined-required"
+                            label="Job Title"
+                            name="itr_name"
+                            value={itr_name}
+                            variant="outlined"
+                            onChange={(e) => setItr_name(e.target.value)}
+                        />
+                    </Grid>
+
+                    <Grid container item>
+                        <TextField
+                            required
+                            id="outlined-required"
+                            label="Stipend provided"
+                            name="stipend"
+                            value={stipend}
+                            variant="outlined"
+                            onChange={(e) => setStipend(e.target.value)}
+                        />
+                    </Grid>
+
+                    <Grid container item>
+                        <TextField
+                            required
+                            id="outlined-required"
+                            label="Required Branch"
+                            name="branch"
+                            value={branch}
+                            variant="outlined"
+                            onChange={(e) => setBranch(e.target.value)}
+                        />
+                    </Grid>
+
+                    <Grid container item>
+                        <TextField
+                            required
+                            id="outlined-required"
+                            label="City"
+                            name="city"
+                            value={city}
+                            variant="outlined"
+                            onChange={(e) => setCity(e.target.value)}
+                        />
+                    </Grid>
+
+                    <Grid container item>
+                        <TextField
+                            required
+                            id="outlined-required"
+                            label="Description"
+                            name="description"
+                            value={description}
+                            variant="outlined"
+                            onChange={(e) => setDescription(e.target.value)}
+                        />
+                    </Grid>
+
+                    <Grid container item>
+                        <TextField
+                            required
+                            id="outlined-required"
+                            label="Minimum GPA"
+                            name="gpa"
+                            value={gpa}
+                            variant="outlined"
+                            onChange={(e) => setGpa(e.target.value)}
+                        />
+                    </Grid>
+
+                    <Grid container item>
+                        <Button
+                            className={classes.button}
+                            variant="contained"
+                            color="primary"
+                            // type="submit"
+                            onClick={onSubmit}
+                        >
+                            Add Internship
+                        </Button>
+                    </Grid>
+                </Grid>
+            </form>
+        </Container>
+    );
+}
+
+export default withStyles(useStyles)(AddInternship);
