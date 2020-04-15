669a60e66840945b8cacdf0bee0f8d5eb9cf1737
Rishi Ravikumar <rishiravi.k98@gmail.com>
2020-04-15 13:59:05 +0530

    Minor changes

--- frontend/src/Components/AddInternship/index.js
+++ frontend/src/Components/AddInternship/index.js
@@ -6,9 +6,7 @@ import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
-import { useGetAndSet, useStoreValue } from "react-context-hook";
-
-const setToken = require("../../util/auth").setToken;
+import {  useStoreValue } from "react-context-hook";
 
 const useStyles = (theme) => ({
     root: {
